b0643022bbc3e1ddcfe3ee2351e76c4b5bd1a358
aboutphilippe <philippemarketing@gmail.com>
2024-08-12 15:01:18 +0200

    ui polish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <button
         onClick={createSession}
-        className="mt-4 p-2 bg-blue-500 text-white rounded"
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-400"
         disabled={loading}
       >
         {loading ? "Loading..." : "Create Session"}
--- src/components/ChatEvents.tsx
+++ src/components/ChatEvents.tsx
@@ -20,19 +20,15 @@ const ChatEvents: React.FC<ChatEventsProps> = ({
       <h3>Received Events for session {sessionId}</h3>
       <div className="h-full overflow-y-auto p-4">
         <ul className="space-y-4">
-          {!events.length && (
-            <li key="waiting" className="flex">
-              <div className="flex gap-2 text-right max-w-xs ml-auto">
-                <p className="bg-neutral-700 text-white p-4 rounded-xl animate-pulse">
-                  ...
-                </p>
-              </div>
-            </li>
-          )}
           {events
             .filter((event) => ["answer", "question"].includes(event.event))
             .map((event, index) => (
-              <li key={index} className="flex">
+              <li
+                key={index}
+                className={`flex ${
+                  event.event === "answer" ? "justify-start" : "justify-end"
+                }`}
+              >
                 {event.event === "answer" && (
                   <div className="flex gap-2 text-left max-w-xs">
                     <p className="bg-neutral-700 text-white p-4 rounded-xl">
@@ -50,12 +46,17 @@ const ChatEvents: React.FC<ChatEventsProps> = ({
               </li>
             ))}
           {loading.isLoading && (
-            <li key="loading" className="flex">
-              <div className="flex gap-2 text-right max-w-xs ml-auto">
+            <li
+              key="loading"
+              className={`${
+                loading.track === "agent" ? "justify-start" : "justify-end"
+              } flex`}
+            >
+              <div className="max-w-xs">
                 <p
                   className={`${
                     loading.track === "agent" ? "bg-neutral-700" : "bg-blue-500"
-                  } text-white p-4 rounded-xl animate-pulse`}
+                  } text-white px-4 py-2 rounded-xl animate-pulse`}
                 >
                   ...
                 </p>
